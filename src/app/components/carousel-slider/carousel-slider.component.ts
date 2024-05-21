0b73e47393bdd4972dc8db24745bcd9b35269d1f
Ranjan Khatiwada <ranjan.kh11@gmail.com>
2024-05-20 22:32:31 -0400

    Fix route error

--- src/app/components/carousel-slider/carousel-slider.component.ts
+++ src/app/components/carousel-slider/carousel-slider.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarouselSliderComponent implements OnInit {
   images: string[] = [
-    'https://images.unsplash.com/photo-1551836022-b06985bceb24?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTcxNDg2MjM5Mnw&ixlib=rb-4.0.3&q=80&w=1080',
-    'https://images.unsplash.com/photo-1602028915047-37269d1a73f7?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTcxNDg2MjM5Mnw&ixlib=rb-4.0.3&q=80&w=1080',
-    'https://images.unsplash.com/photo-1555737202-2e86ace3b68f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTcxNDg2MjM5Mnw&ixlib=rb-4.0.3&q=80&w=1080'
+    // 'https://images.unsplash.com/photo-1551836022-b06985bceb24?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTcxNDg2MjM5Mnw&ixlib=rb-4.0.3&q=80&w=1080',
+    // 'https://images.unsplash.com/photo-1602028915047-37269d1a73f7?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTcxNDg2MjM5Mnw&ixlib=rb-4.0.3&q=80&w=1080',
+    // 'https://images.unsplash.com/photo-1555737202-2e86ace3b68f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTcxNDg2MjM5Mnw&ixlib=rb-4.0.3&q=80&w=1080',
+    'assets/gallery/Gallery1.jpg',
+    'assets/gallery/Gallery 2.JPEG',
+    'assets/gallery/Gallery 3.JPG',
+    'assets/gallery/Gallery 4.JPEG'
+
   ];
   currentIndex: number = 0;
 
